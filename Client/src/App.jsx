ee5bbd8837e1bf781f1be337b044137f14bb94b4
000626574705 <shreetikagmm123@gmail.com>
2024-07-13 13:24:51 +0530

    Backend  done!

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,27 +1,31 @@
+import React from "react";
+import Home from "./home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
+import Signup from "./components/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
+import Contact from "./components/Contact";
 
-
-import Home from "./home/Home.jsx";
-import Courses from "./course/Courses.jsx";
-import { Route ,Routes} from 'react-router-dom';
-import Signup from "./components/Signup.jsx";
-function App ()  {
+function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
-  <>
- 
-<div className="dark:bg-slate-900 dark:text-white">
-<Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path="/courses" element={<Courses/>}/>
-  <Route path="/signup" element={<Signup/>}/>
- </Routes>
-</div>
- 
- 
-
-  </>
-
-  )
+    <>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/courses"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Toaster />
+      </div>
+    </>
+  );
 }
 
 export default App;
-
--- Client/src/home/Home.jsx
+++ Client/src/home/Home.jsx
@@ -1,19 +1,18 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Banner from "../components/Banner";
+import Freebook from "../components/Freebook";
+import Footer from "../components/Footer";
 
-import Navbar from '../components/Navbar'
-import Banner from '../components/Banner'
-import Footer from '../components/Footer'
-import Freebook from '../components/Freebook'
 function Home() {
   return (
     <>
-    <div className='dark:bg-slate-900 dark:text-white'>
-      <Navbar/>
-      <Banner/>
-      <Freebook/>
-      <Footer/>
-      </div>
+      <Navbar />
+      <Banner />
+      <Freebook />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
